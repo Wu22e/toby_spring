dba30a876177d8b2e8d2a3dd61733b3222b5c3af
Wu22e <gulb1602@gmail.com>
2021-10-15 23:09:06 +0900

    refactoring : 상속을 통한 확장

---
+++ toby_ch1/src/springbook/user/dao/NUserDao.java
@@ -0,0 +1,15 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class NUserDao extends UserDao {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        // N 사 DB connection 생성 코드
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?characterEncoding=UTF-8", "spring",
+                "book");
+        return c;
+    }
+}
--- toby_ch1/src/springbook/user/dao/UserDao.java
+++ toby_ch1/src/springbook/user/dao/UserDao.java
@@ -4,7 +4,7 @@ import springbook.user.domain.User;
 
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
         Connection c = getConnection();
@@ -42,10 +42,5 @@ public class UserDao {
         return user;
     }
 
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?characterEncoding=UTF-8", "spring",
-                "book");
-        return c;
-    }
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 }
