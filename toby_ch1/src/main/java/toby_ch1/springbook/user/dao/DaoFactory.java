8d456dd2d7bef7166fe474edfe63bf046d77c1e7
Wu22e <gulb1602@gmail.com>
2021-10-22 02:09:26 +0900

    refactoring : 애플리케이션 컨텍스트를 위한 애노테이션 추가 및 테스트

--- toby_ch1/src/main/java/toby_ch1/springbook/user/dao/DaoFactory.java
+++ toby_ch1/src/main/java/toby_ch1/springbook/user/dao/DaoFactory.java
@@ -3,8 +3,10 @@ package toby_ch1.springbook.user.dao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// 설정정보를 위한 애노테이션
 @Configuration
 public class DaoFactory {
+    // 오브젝트로 관리될 빈
     @Bean
     public UserDao userDao() {
         UserDao dao = new UserDao(connectionMaker());
